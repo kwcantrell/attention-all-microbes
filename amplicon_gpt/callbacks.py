a8b7143ca886a808111537220525e8c47484e036
Alex Cagle <ajcagle@ucsd.edu>
2024-02-15 11:00:02 -0800

    Fixing callbacks.py

--- amplicon_gpt/callbacks.py
+++ amplicon_gpt/callbacks.py
@@ -4,7 +4,7 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import sklearn
-from tensorboard.plugins import projector
+# from tensorboard.plugins import projector
 from amplicon_gpt.losses import _pairwise_distances
 from skbio.stats.distance import DistanceMatrix
 import skbio.stats.ordination
@@ -22,14 +22,14 @@ def mean_absolute_error(dataset, model, fname, s_type):
     pred_age = tf.squeeze(model.predict(dataset)).numpy()
     true_age = np.concatenate([tf.squeeze(ys).numpy() for (_, ys) in dataset])
     mae, h = mean_confidence_interval(np.abs(true_age - pred_age))
-    
+
     min_x = 15
     max_x = np.max(true_age) + 2
     coeff = np.polyfit(true_age, pred_age, deg=1)
     p = np.poly1d(coeff)
     xx = np.linspace(min_x, max_x, 1000)
     yy = p(xx)
-    
+
     plt.figure(figsize=(4, 4))
     plt.rcParams.update({
         'text.usetex': True
@@ -48,13 +48,14 @@ def mean_absolute_error(dataset, model, fname, s_type):
     plt.close()
 
 class BaseCheckpoint(tf.keras.callbacks.Callback):
-    def __init__(self, root_path, dataset, s_type, steps_per_checkpoint=5, **kwargs):
+    def __init__(self, root_path, dataset, s_type, steps_per_checkpoint=5,
+                 **kwargs):
         super().__init__()
 
         self.dataset = dataset
         self.root_path = root_path
-        self.model_path = os.path.join(self.root_path, 'model.keras') 
-        self.figure_path = os.path.join(self.root_path, 'figures') 
+        self.model_path = os.path.join(self.root_path, 'model.keras')
+        self.figure_path = os.path.join(self.root_path, 'figures')
         self.cur_step = 1
         self.total_mae = 0
         self.s_type = s_type
@@ -64,12 +65,13 @@ class BaseCheckpoint(tf.keras.callbacks.Callback):
             os.makedirs(self.figure_path)
 
 class MAE_Scatter(tf.keras.callbacks.Callback):
-    def __init__(self, root_path, dataset, s_type, title, steps_per_checkpoint=5, **kwargs):
+    def __init__(self, root_path, dataset, s_type, title,
+                 steps_per_checkpoint=5, **kwargs):
         super().__init__()
         self.dataset = dataset
         self.root_path = root_path
-        self.model_path = os.path.join(self.root_path, 'model.keras') 
-        self.figure_path = os.path.join(self.root_path, 'figures') 
+        self.model_path = os.path.join(self.root_path, 'model.keras')
+        self.figure_path = os.path.join(self.root_path, 'figures')
         self.cur_step = 0
         self.total_mae = 0
         self.s_type = s_type
@@ -83,14 +85,17 @@ class MAE_Scatter(tf.keras.callbacks.Callback):
         if self.cur_step % 5 == 0:
             self.total_mae += 1
             self.model.save(self.model_path, save_format='keras')
-            mean_absolute_error(self.dataset, self.model, fname=os.path.join(self.figure_path,
-                                f'MAE-{self.title}-{self.total_mae}.png'), s_type=self.s_type)
+            mean_absolute_error(self.dataset, self.model,
+                                fname=os.path.join(self.figure_path,
+                                f'MAE-{self.title}-{self.total_mae}.png'),
+                                s_type=self.s_type)
             self.cur_step = 0
         self.cur_step += 1
         return super().on_epoch_end(epoch, logs)
-    
+
 class ProjectEncoder(tf.keras.callbacks.Callback):
-    def __init__(self, data, model_path, pred_pcoa_path, true_pcoa_path, table_path, tree_path, num_samples, batch_size, **kwargs):
+    def __init__(self, data, model_path, pred_pcoa_path, true_pcoa_path,
+                 table_path, tree_path, num_samples, batch_size, **kwargs):
         super().__init__()
         self.batch_size = batch_size
         self.data = data
@@ -105,9 +110,11 @@ class ProjectEncoder(tf.keras.callbacks.Callback):
 
     def _log_epoch_data(self):
         tf.print('loggin data...')
-        self.model.save(os.path.join(self.model_path, 'encoder.keras'), save_format='keras')
-        total_samples = int(self.table.shape[1] / self.batch_size) * self.batch_size
-        
+        self.model.save(os.path.join(self.model_path, 'encoder.keras'),
+                save_format='keras')
+        total_samples = int(self.table.shape[1] / self.batch_size)
+                            * self.batch_size
+
         sample_indices = np.arange(total_samples)
         np.random.shuffle(sample_indices)
         sample_indices = sample_indices[:self.num_samples]
@@ -115,12 +122,17 @@ class ProjectEncoder(tf.keras.callbacks.Callback):
 
         pred = tf.gather(pred, sample_indices)
         distances = _pairwise_distances(pred, squared=False)
-        pred_unifrac_distances = DistanceMatrix(distances.numpy(), self.table.ids(axis='sample')[sample_indices], validate=False)
-        pred_pcoa = skbio.stats.ordination.pcoa(pred_unifrac_distances, method='fsvd', number_of_dimensions=3, inplace=True)
+        pred_unifrac_distances = DistanceMatrix(distances.numpy(),
+                self.table.ids(axis='sample')[sample_indices], validate=False)
+        pred_pcoa = skbio.stats.ordination.pcoa(pred_unifrac_distances,
+                method='fsvd', number_of_dimensions=3, inplace=True)
         pred_pcoa.write(self.pred_pcoa_path)
 
-        true_unifrac_distances = unweighted(self.table_path, self.tree_path).filter(self.table.ids(axis='sample')[sample_indices])
-        true_pcoa = skbio.stats.ordination.pcoa(true_unifrac_distances, method='fsvd', number_of_dimensions=3, inplace=True)
+        true_unifrac_distances = unweighted(
+                self.table_path, self.tree_path
+                ).filter(self.table.ids(axis='sample')[sample_indices])
+        true_pcoa = skbio.stats.ordination.pcoa(true_unifrac_distances,
+                method='fsvd', number_of_dimensions=3, inplace=True)
         true_pcoa.write(self.true_pcoa_path)
 
     def on_epoch_end(self, epoch, logs=None):
@@ -128,14 +140,16 @@ class ProjectEncoder(tf.keras.callbacks.Callback):
             self._log_epoch_data()
             self.cur_step = 0
         self.cur_step += 1
-    
+
+
 class Accuracy(tf.keras.callbacks.Callback):
-    def __init__(self, root_path, dataset, s_type, steps_per_checkpoint=5, **kwargs):
+    def __init__(self, root_path, dataset, s_type, steps_per_checkpoint=5,
+                 **kwargs):
         super().__init__()
         self.dataset = dataset
         self.root_path = root_path
-        self.model_path = os.path.join(self.root_path, 'model.keras') 
-        self.figure_path = os.path.join(self.root_path, 'figures') 
+        self.model_path = os.path.join(self.root_path, 'model.keras')
+        self.figure_path = os.path.join(self.root_path, 'figures')
         self.cur_step = 0
         self.total_mae = 0
         self.s_type = s_type
@@ -149,13 +163,17 @@ class Accuracy(tf.keras.callbacks.Callback):
         if self.cur_step % self.steps_per_checkpoint == 0:
             self.total_mae += 1
             self.model.save(self.model_path, save_format='keras')
-            
+
             pred_cat = tf.squeeze(self.model.predict(self.dataset)).numpy()
-            true_cat = np.concatenate([tf.squeeze(ys).numpy() for (_, ys) in self.dataset])
-            
+            true_cat = np.concatenate([tf.squeeze(ys).numpy() for (_, ys) in
+                                       self.dataset])
+
 
             def plot_prc(name, labels, predictions, **kwargs):
-                precision, recall, _ = sklearn.metrics.precision_recall_curve(labels, predictions)
+                precision, recall, _ = sklearn.metrics.precision_recall_curve(
+                    labels,
+                    predictions
+                )
 
                 plt.plot(precision, recall, label=name, linewidth=2, **kwargs)
                 plt.xlabel('Precision')
@@ -164,7 +182,7 @@ class Accuracy(tf.keras.callbacks.Callback):
                 ax = plt.gca()
                 ax.set_aspect('equal')
                 fname=os.path.join(self.figure_path,
-                                f'auc-{self.total_mae}.png')
+                                   f'auc-{self.total_mae}.png')
                 plt.savefig(fname)
                 plt.close('all')
 
@@ -176,13 +194,13 @@ class Accuracy(tf.keras.callbacks.Callback):
                 plt.plot(100*fp, 100*tp, label=name, linewidth=2, **kwargs)
                 plt.xlabel('False positives [%]')
                 plt.ylabel('True positives [%]')
-                plt.xlim([-0.5,20])
-                plt.ylim([80,100.5])
+                plt.xlim([-0.5, 20])
+                plt.ylim([80, 100.5])
                 plt.grid(True)
                 ax = plt.gca()
                 ax.set_aspect('equal')
-                fname=os.path.join(self.figure_path,
-                                f'roc-{self.total_mae}.png')
+                fname = os.path.join(self.figure_path,
+                                   f'roc-{self.total_mae}.png')
                 plt.savefig(fname)
             plot_roc('ROC', true_cat, pred_cat)
 
