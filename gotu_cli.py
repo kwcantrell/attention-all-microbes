2f5b57966a8ca445e316c0d261b69e0239622db1
Jonathan Kirkland <jokirkland@ucsd.edu>
2024-08-06 13:46:49 -0700

    base cli

--- gotu_cli.py
+++ gotu_cli.py
@@ -1,106 +1,176 @@
 """gotu.py"""
 
 import os
-import time
+from datetime import datetime
+
 import click
+import tensorflow as tf
+
+import aam._parameter_descriptions as desc
+from aam.callbacks import SaveModel
+from aam.data_utils import load_data
+from aam.utils import LRDecrease
+from attention_regression.callbacks import MAE_Scatter
+from gotu.gotu_model import GOTUModel
 
 
-from gotu.gotu_data_utils import (
-    create_training_dataset,
-    create_prediction_dataset,
-    save_dataset,
-    save_gotu_dict,
-    load_dataset,
-    load_model_dict,
-)
-from gotu.gotu_model import (
-    gotu_classification,
-    gotu_predict,
-    run_gotu_predictions,
-    run_gotu_training
-    )
-
-# TODO: Add catch statements to cli to validate file/fp's
-
-
-@click.command(help="train ASV to GOTU model")
-@click.argument("dataset-path", type=click.Path(exists=True))
-@click.option("--model_fp", type=click.Path(), help="File path to save the model.")
-def run_gotu_training_cli(dataset_path, model_fp):
-    training_fp = f"{dataset_path}/training_set"
-    val_fp = f"{dataset_path}/validation_set"
-    training_dataset = load_dataset(training_fp)
-    validation_dataset = load_dataset(val_fp)
-    run_gotu_training(
-        training_dataset, validation_dataset, model_fp=model_fp
-    )
-
-
-@click.command(help="Run predictions with previously trained models")
-@click.argument("asv_fp", type=click.Path(exists=True))
-@click.argument("gotu_fp", type=click.Path(exists=True))
-@click.argument("model_fp", type=click.Path(exists=True))
-@click.argument("pred_out_path", type=click.Path())
-def run_gotu_predictions_cli(asv_fp, gotu_fp, model_fp, pred_out_path):
-    run_gotu_predictions(asv_fp, gotu_fp, model_fp, pred_out_path)
-
-
-@click.command(help="Creates tokenized training/validation datasets")
-@click.argument("asv-fp", type=click.Path(exists=True))
-@click.argument("gotu-fp", type=click.Path(exists=True))
-@click.option("--name", default="a2g")
-@click.option("--train-split", default=0.8, show_default=True)
-@click.option("--outpath", type=click.Path(exists=False))
-def create_training_set_cli(asv_fp, gotu_fp, name, train_split, outpath):
-    timestamp = time.strftime("%Y%m%d-%H%M")
-    full_dir = f"{outpath}/{timestamp}-{name}"
-
-    try:
-        os.mkdir(full_dir)
-    except NotADirectoryError as e:
-        print(f"Failed to create out folder directory: {e}")
-
-    training_fp = f"training_set"
-    val_fp = f"validation_set"
-    dict_fp = f"gotu_dict"
-    training_batched, val_batched, gotu_dict = create_training_dataset(
-        asv_fp, gotu_fp, train_split
-    )
-    save_dataset(training_batched, full_dir, training_fp)
-    save_dataset(val_batched, full_dir, val_fp)
-    save_gotu_dict(gotu_dict, full_dir, dict_fp)
-
-@click.command(help="Creates tokenized dataset for running predictions")
-@click.argument("asv-fp", type=click.Path(exists=True))
-@click.argument("gotu-fp", type=click.Path(exists=True))
-@click.option("--name", default="a2g")
-@click.option("--outpath", type=click.Path(exists=False))
-def create_training_set_cli(asv_fp, gotu_fp, name, train_split, outpath):
-    timestamp = time.strftime("%Y%m%d-%H%M")
-    full_dir = f"{outpath}/{timestamp}-{name}"
-
-    try:
-        os.mkdir(full_dir)
-    except NotADirectoryError as e:
-        print(f"Failed to create out folder directory: {e}")
-    
-    dataset_fp = f"pred-dataset"
-    dict_fp = f"gotu_dict"
-    training_batched, val_batched, gotu_dict = create_training_dataset(
-        asv_fp, gotu_fp, train_split
-    )
-    save_dataset(training_batched, full_dir, dataset_fp)
-    save_gotu_dict(gotu_dict, full_dir, dict_fp)
-    
-    
 @click.group()
-def cli():
+class cli:
     pass
 
 
-cli.add_command(run_gotu_training_cli, "train")
-cli.add_command(run_gotu_predictions_cli, "predict")
-cli.add_command(create_training_set_cli, "format-training-data")
+def _aam_globals():
+    return {"feature-attention-methods": ["add_features", "mask_features", "none"]}
+
+
+aam_globals = _aam_globals()
+
+
+@cli.command()
+@click.option("--i-table-path", required=True, help=desc.TABLE_DESC, type=click.Path(exists=True))
+@click.option(
+    "--i-gotu-path",
+    required=True,
+    help=desc.TABLE_DESC,
+    type=click.Path(exists=True),
+)
+@click.option("--i-max-bp", required=True, type=int)
+@click.option(
+    "--p-missing-samples",
+    default="error",
+    type=click.Choice(["error", "ignore"], case_sensitive=False),
+    help=desc.MISSING_SAMPLES_DESC,
+)
+@click.option("--p-batch-size", default=8, show_default=True, type=int)
+@click.option("--p-epochs", default=1000, show_default=True, type=int)
+@click.option("--p-repeat", default=5, show_default=True, type=int)
+@click.option("--p-dropout", default=0.01, show_default=True, type=float)
+@click.option("--p-token-dim", default=512, show_default=True, type=int)
+@click.option("--p-ff-d-model", default=128, show_default=True, type=int)
+@click.option("--p-ff-clr", default=64, show_default=True, type=int)
+@click.option("--p-pca-heads", default=8, show_default=True, type=int)
+@click.option("--p-enc-layers", default=2, show_default=True, type=int)
+@click.option("--p-enc-heads", default=8, show_default=True, type=int)
+@click.option("--p-include-random", default=True, show_default=True, type=bool)
+@click.option("--p-lr", default=0.01, show_default=True, type=float)
+@click.option("--p-report-back-after", default=5, show_default=True, type=int)
+@click.option("--p-base-model-path", required=True, type=click.Path(exists=True))
+@click.option("--p-output-dir", required=True)
+def sequence2sequence(
+    i_table_path: str,
+    i_max_bp: int,
+    p_missing_samples: str,
+    p_batch_size: int,
+    p_epochs: int,
+    p_repeat: int,
+    p_dropout: float,
+    p_token_dim: int,
+    p_feature_attention_method: str,
+    p_features_to_add_rate: float,
+    p_ff_d_model: int,
+    p_ff_clr: int,
+    p_pca_heads: int,
+    p_enc_layers: int,
+    p_enc_heads: int,
+    p_include_random: bool,
+    p_lr: float,
+    p_report_back_after: int,
+    p_base_model_path: str,
+    p_output_dir: str,
+    p_tensorboard: bool,
+):
+    if not os.path.exists(p_output_dir):
+        os.makedirs(p_output_dir)
+
+    figure_path = os.path.join(p_output_dir, "figures")
+    if not os.path.exists(figure_path):
+        os.makedirs(figure_path)
+
+    data_obj = load_data(
+        i_table_path,
+        i_max_bp,
+        p_batch_size,
+        repeat=p_repeat,
+        shuffle_samples=True,
+        missing_samples_flag=p_missing_samples,
+        train_percent=0.8,
+    )
+
+    train_size = data_obj["training_dataset"].cardinality().numpy()
+
+    base_model = tf.keras.models.load_model(p_base_model_path, compile=False)
+    base_model = tf.keras.Model(
+        inputs=base_model.get_layer("nucleotide_embedding").input,
+        outputs=base_model.get_layer("nucleotide_embedding").output,
+        name="unifrac_model",
+    )
+    base_model.trainable = False
+
+    num_gotus = data_obj["num_gotus"]
+
+    gotu_model = GOTUModel(
+        base_model=base_model,
+        dropout_rate=p_dropout,
+        batch_size=p_batch_size,
+        max_bp=i_max_bp,
+        num_gotus=num_gotus,
+        pca_hidden_dim=base_model.pca_hidden_dim,
+        pca_heads=p_pca_heads,
+        pca_layers=base_model.pca_layers,
+        count_ff_dim=p_ff_clr,
+        num_layers=p_enc_layers,
+        num_attention_heads=p_enc_heads,
+        dff=p_ff_d_model,
+        d_model=p_token_dim,
+    )
+
+    optimizer = tf.keras.optimizers.Adam(
+        learning_rate=LRDecrease(),
+        beta_1=0.9,
+        beta_2=0.98,
+        epsilon=1e-9,
+    )
+
+    gotu_model.compile(
+        optimizer=optimizer,
+        loss=gotu_model.regresssion_loss,
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+    )
+
+    gotu_model.build(input_shape=[(p_batch_size, None, 150), (p_batch_size, None, 1)])
+    gotu_model.summary()
+
+    # core_callbacks = [
+    #     tf.keras.callbacks.ReduceLROnPlateau(
+    #         monitor="loss", factor=0.8, patience=10, min_lr=0.0001
+    #     ),
+    #     tf.keras.callbacks.EarlyStopping(monitor="loss", patience=20),
+    #     tf.keras.callbacks.ModelCheckpoint(
+    #         filepath=os.path.join(p_output_dir, "best_model.h5"),
+    #         save_best_only=True,
+    #         monitor="val_loss",
+    #         mode="min",
+    #     ),
+    # ]
+
+    # gotu_model.fit(
+    #     data_obj["training_dataset"],
+    #     validation_data=data_obj["validation_dataset"],
+    #     callbacks=core_callbacks,
+    #     epochs=p_epochs,
+    # )
+
+
+def main():
+    gpus = tf.config.list_physical_devices("GPU")
+    if len(gpus) > 0:
+        tf.config.experimental.set_memory_growth(gpus[0], True)
+    tf.keras.mixed_precision.set_global_policy("mixed_float16")
+
+    # Equivalent to the two lines above
+
+    cli()
+
 
 if __name__ == "__main__":
-    cli()
+    main()
