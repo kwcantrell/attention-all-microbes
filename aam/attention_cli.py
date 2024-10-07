softmax fix

aam/attention_cli.py
@@ -15,7 +15,7 @@ from aam.callbacks import (
     _mean_absolute_error,
 )
 from aam.cv_utils import CVModel, EnsembleModel
-from aam.losses import ImbalancedCategoricalCrossEntropy, ImbalancedMSE
+from aam.losses import ImbalancedCategoricalCrossEntropy
 from aam.transfer_nuc_model import TransferLearnNucleotideModel
 from aam.unifrac_model import UnifracModel
 
@@ -43,8 +43,8 @@ MISSING_SAMP_DESC = 'How to handle missing samples in metadata. "error" will fai
 @click.option("--p-dropout", default=0.0, show_default=True, type=float)
 @click.option("--p-ff-d-model", default=128, show_default=True, type=int)
 @click.option("--p-pca-heads", default=8, show_default=True, type=int)
-@click.option("--p-enc-layers", default=2, show_default=True, type=int)
-@click.option("--p-enc-heads", default=8, show_default=True, type=int)
+@click.option("--p-enc-layers", default=4, show_default=True, type=int)
+@click.option("--p-enc-heads", default=4, show_default=True, type=int)
 @click.option("--p-penalty", default=0.01, type=float)
 @click.option("--p-patience", default=10, show_default=True, type=int)
 @click.option("--p-early-stop-warmup", default=50, show_default=True, type=int)
@@ -260,7 +260,7 @@ def fit_sample_regressor(
 
     print(len(test_indices), len(fold_indices))
 
-    def _get_fold(indices, shuffle):
+    def _get_fold(indices, shuffle, shift=None, scale=None):
         fold_ids = ids[indices]
         table_fold = table.filter(fold_ids, axis="sample", inplace=False)
         df_fold = df[df.index.isin(fold_ids)]
@@ -272,6 +272,8 @@ def fit_sample_regressor(
             shuffle_samples=shuffle,
             batch_size=p_batch_size,
             max_token_per_sample=p_asv_limit,
+            shift=shift,
+            scale=scale,
         )
         return data
 
@@ -280,7 +282,9 @@ def fit_sample_regressor(
     models = []
     for i, (train_ind, val_ind) in enumerate(kfolds.split(fold_indices)):
         train_data = _get_fold(train_ind, shuffle=True)
-        val_data = _get_fold(val_ind, shuffle=False)
+        val_data = _get_fold(
+            val_ind, shuffle=False, shift=train_data["shift"], scale=train_data["scale"]
+        )
         with open(os.path.join(model_path, f"f{i}_val_ids.txt"), "w") as f:
             for id in ids[val_ind]:
                 f.write(id + "\n")
@@ -295,7 +299,8 @@ def fit_sample_regressor(
             penalty=p_penalty,
             dropout=p_dropout,
         )
-        loss = ImbalancedMSE(train_data["max_density"])
+        # loss = ImbalancedMSE(train_data["max_density"])
+        loss = tf.keras.losses.Huber()
         fold_label = i + 1
         model_cv = CVModel(
             model,
@@ -308,7 +313,7 @@ def fit_sample_regressor(
             loss,
             p_epochs,
             os.path.join(model_path, f"model_f{fold_label}.keras"),
-            metric="mae",
+            metric="loss",
             patience=p_patience,
             early_stop_warmup=p_early_stop_warmup,
             callbacks=[

aam/callbacks.py
@@ -73,13 +73,12 @@ class MeanAbsoluteError(tf.keras.callbacks.Callback):
         self.dataset = dataset
         self.output_dir = output_dir
         self.report_back = report_back
-        self.best_weights = None
         self.best_metric = None
         self.monitor = monitor
 
     def on_epoch_end(self, epoch, logs=None):
         metric = logs[self.monitor]
-        if self.best_weights is None or self.best_metric > metric:
+        if self.best_metric is None or self.best_metric > metric:
             y_pred, y_true = self.model.predict(self.dataset)
             _mean_absolute_error(y_pred, y_true, self.output_dir)
             self.best_metric = metric

aam/layers.py
@@ -222,7 +222,6 @@ class NucleotideAttention(tf.keras.layers.Layer):
         # self.epsilon = 0.000001
         self.epsilon = 1e-3
         self.intermediate_ff = intermediate_ff
-
         self.pos_emb = tfm.nlp.layers.PositionEmbedding(
             max_length=self.max_bp + 1, seq_axis=2, name="nuc_pos"
         )
@@ -247,7 +246,6 @@ class NucleotideAttention(tf.keras.layers.Layer):
             attention_input = self.attention_layers[layer_idx](
                 attention_input, training=training
             )
-
         output = self.output_normalization(attention_input)
         return output
 
@@ -333,7 +331,7 @@ class NucleotideAttentionBlock(tf.keras.layers.Layer):
         )
 
         scaled_dot_tensor = tf.multiply(dot_tensor, 1 / self.scale_dot_factor)
-        softmax_tensor = tf.keras.activations.softmax(scaled_dot_tensor, axis=-2)
+        softmax_tensor = tf.keras.activations.softmax(scaled_dot_tensor, axis=-1)
 
         # [B, A, H, N, N] => [B, A, H, N, S]
         attention_output = tf.matmul(softmax_tensor, wv_tensor)
@@ -420,9 +418,7 @@ class CountEncoder(tf.keras.layers.Layer):
         # #     initializer=tf.keras.initializers.GlorotNormal(),
         # #     trainable=True,
         # # )
-        # self.upscale_in_ff = tf.keras.layers.Dense(
-        #     128, use_bias=True, activation="relu", dtype=dtype
-        # )
+        self.upscale_in_ff = tf.keras.layers.Dense(128, use_bias=False, dtype=dtype)
         # self.upscale_out_ff = tf.keras.layers.Dense(128, use_bias=True, dtype=dtype)
 
         self.norm = tf.keras.layers.LayerNormalization(
@@ -448,6 +444,7 @@ class CountEncoder(tf.keras.layers.Layer):
         count_embeddings = tf.squeeze(inputs, axis=-1)
         count_embeddings = self.embedding(float_mask(count_embeddings, dtype=tf.int32))
         count_embeddings = tf.multiply(count_embeddings, rel_abundance)
+        # count_embeddings = self.upscale_in_ff(count_embeddings)
         count_embeddings = count_embeddings + self.count_ranks(count_embeddings)
         # count_embeddings = self.norm(count_embeddings)
         return count_embeddings

aam/transfer_data_utils.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy.stats
 import tensorflow as tf
 
 
@@ -49,6 +48,8 @@ def load_data(
     missing_samples_flag=None,
     max_token_per_sample=512,
     batch_size=8,
+    shift=None,
+    scale=None,
 ):
     def _get_table_data(table_data):
         coo = table_data.transpose().matrix_data.tocoo()
@@ -75,14 +76,18 @@ def load_data(
         target_data = tf.expand_dims(target_data, axis=-1)
         target_dataset = tf.data.Dataset.from_tensor_slices(target_data)
     else:
-        shift = 0
-        scale = 1  # np.max(target_data)
+        target_data = target_data.to_numpy().reshape((-1, 1))
+        if shift is None and scale is None:
+            shift = np.mean(target_data)
+            scale = np.std(target_data)
         target_data = (target_data - shift) / scale
-        density = scipy.stats.gaussian_kde(target_data)
-        y = density(target_data).astype(np.float32)
-        max_density = np.max(y)
-        target_data = tf.expand_dims(target_data, axis=-1)
-        y = tf.expand_dims(y, axis=-1)
+        print(target_data)
+        y = target_data
+        # density = scipy.stats.gaussian_kde(target_data)
+        # y = density(target_data).astype(np.float32)
+        # max_density = np.max(y)
+        # target_data = tf.expand_dims(target_data, axis=-1)
+        # y = tf.expand_dims(y, axis=-1)
         target_dataset = tf.data.Dataset.from_tensor_slices(target_data)
         y_dataset = tf.data.Dataset.from_tensor_slices(y)
         target_dataset = tf.data.Dataset.zip(target_dataset, y_dataset)

aam/transfer_nuc_model.py
@@ -55,10 +55,11 @@ class TransferLearnNucleotideModel(tf.keras.Model):
         self.count_activation = tf.keras.layers.Activation("linear", dtype=tf.float32)
         self.count_tracker = tf.keras.metrics.Mean()
 
-        self.transfer_encoder = tfm.nlp.models.TransformerDecoder(
+        # self.transfer_encoder = tfm.nlp.models.TransformerDecoder(
+        self.transfer_encoder = tfm.nlp.models.TransformerEncoder(
             num_layers=4,
             num_attention_heads=4,
-            intermediate_size=64,
+            intermediate_size=1024,
             dropout_rate=self.dropout,
         )
 
@@ -94,6 +95,7 @@ class TransferLearnNucleotideModel(tf.keras.Model):
         return evaluated_metrics[metric_index]
 
     def _compute_loss(self, y_true, outputs):
+        y, _ = y_true
         _, count_pred, y_pred, counts = outputs
 
         # count mask
@@ -114,8 +116,8 @@ class TransferLearnNucleotideModel(tf.keras.Model):
         # abundance_loss = tf.square(1 - pred_total_abundance)
         # abundance_loss = self.penalty * tf.reduce_mean(abundance_loss)
 
-        # num_counts = tf.reduce_sum(count_mask, axis=-1, keepdims=True)
-        # num_counts = tf.ensure_shape(num_counts, [None, 1])
+        num_counts = tf.reduce_sum(count_mask, axis=-1, keepdims=True)
+        num_counts = tf.ensure_shape(num_counts, [None, 1])
 
         # count mse
         count_loss = tf.math.square(counts - count_pred) * count_mask
@@ -123,7 +125,8 @@ class TransferLearnNucleotideModel(tf.keras.Model):
         count_loss = self.penalty * tf.reduce_mean(count_loss)  # + abundance_loss
 
         y_pred = tf.ensure_shape(y_pred, [None, 1])
-        target_loss = tf.reduce_mean(self.loss(y_true, y_pred))
+        # target_loss = tf.reduce_mean(self.loss(y_true, y_pred))
+        target_loss = tf.reduce_mean(self.loss(y, y_pred))
         reg_loss = tf.reduce_mean(self.losses)
         return (
             tf.reduce_mean(target_loss) + tf.reduce_sum(count_loss),
@@ -223,13 +226,21 @@ class TransferLearnNucleotideModel(tf.keras.Model):
             randomly_mask_nucleotides=False,
             training=False,
         )
-        asv_embeddings = tf.cast(asv_embeddings, dtype=self.compute_dtype)
+        # asv_embeddings = tf.cast(asv_embeddings, dtype=self.compute_dtype)
+
+        extended_counts = tf.cast(extended_counts, dtype=tf.float32)
+        count_sums = tf.reduce_sum(extended_counts, axis=1, keepdims=True)
+        rel_abundance = extended_counts / count_sums
+        rel_abundance = tf.pad(
+            rel_abundance, [[0, 0], [0, 1], [0, 0]], constant_values=1
+        )
+        count_embeddings = asv_embeddings * rel_abundance
         # asv_embeddings = asv_embeddings + self.pos_embeddings(asv_embeddings)
 
-        count_embeddings = self.count_encoder(
-            extended_counts, count_mask=count_mask, training=training
-        )
-        count_embeddings = tf.cast(count_embeddings, dtype=self.compute_dtype)
+        # count_embeddings = self.count_encoder(
+        #     extended_counts, count_mask=count_mask, training=training
+        # )
+        # count_embeddings = tf.cast(count_embeddings, dtype=self.compute_dtype)
 
         # add <SAMPLE> token empbedding
         asv_shape = tf.shape(count_embeddings)
@@ -239,23 +250,23 @@ class TransferLearnNucleotideModel(tf.keras.Model):
         sample_emb_shape[0] = batch_len
         sample_emb_shape[-1] = emb_len
         transfer_token = tf.broadcast_to(self.transfer_token, sample_emb_shape)
-        transfer_token = tf.cast(transfer_token, dtype=self.compute_dtype)
+        transfer_token = tf.cast(transfer_token, dtype=tf.float32)
         count_embeddings = tf.concat([count_embeddings, transfer_token], axis=1)
 
         embeddings = count_embeddings  # + self.pos_embeddings(count_embeddings)
 
         # mask pad values out
-        count_mask = tf.pad(count_mask, [[0, 0], [0, 1], [0, 0]], constant_values=1)
+        count_mask = tf.pad(count_mask, [[0, 0], [0, 2], [0, 0]], constant_values=1)
 
         # asv + count attention
         count_attention_mask = tf.matmul(count_mask, count_mask, transpose_b=True)
         cross_attention_mask = tf.linalg.band_part(count_attention_mask, -1, 0)
 
         embeddings = self.transfer_encoder(
-            asv_embeddings,
+            # asv_embeddings,
             embeddings,
-            self_attention_mask=count_attention_mask > 0,
-            cross_attention_mask=count_attention_mask > 0,
+            attention_mask=count_attention_mask > 0,
+            # cross_attention_mask=count_attention_mask > 0,
             training=training,
         )
 
@@ -264,7 +275,7 @@ class TransferLearnNucleotideModel(tf.keras.Model):
         target_out = self.transfer_dropout(target_out)
         target_out = self.transfer_ff(target_out)
 
-        count_embeddings = embeddings[:, :-1, :]
+        count_embeddings = embeddings[:, :-2, :]
         count_pred = self.count_intermediate(count_embeddings)
         count_pred = self.count_dropout(count_pred)
         count_pred = tf.squeeze(self.count_out(count_pred), axis=-1)