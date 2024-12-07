cf212f14ef3ddd6590a0d448f6dd49ef63bb646c
kcantrel <kcantrel@ucsd.edu>
2024-12-06 22:32:24 -0800

    learned pooling

--- aam/models/multihead_attention_pooling.py
+++ aam/models/multihead_attention_pooling.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 
+from aam.models.attention_pooling import AttentionPooling
+
 
 class MultiHeadAttentionPooling(tf.keras.layers.Layer):
     def __init__(self):
@@ -7,6 +9,7 @@ class MultiHeadAttentionPooling(tf.keras.layers.Layer):
         self.query = tf.keras.layers.Dense(8, use_bias=False)
         self.dropout = tf.keras.layers.Dropout(0.1)
         self.norm = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.pool = AttentionPooling()
 
     def call(self, inputs, mask=None, training=False):
         # Compute attention scores
@@ -29,7 +32,9 @@ class MultiHeadAttentionPooling(tf.keras.layers.Layer):
         attention_weights = tf.expand_dims(attention_weights, axis=-1)  # [B, H, T, 1]
 
         inputs = tf.expand_dims(inputs, axis=1)  # [B, 1, T, D]
-        pooled_output = tf.reduce_sum(inputs * attention_weights, axis=2)  # [B, H, D]
+        pooled_output = self.norm(
+            tf.reduce_sum(inputs * attention_weights, axis=2)
+        )  # [B, H, D]
 
         # Apply normalization
-        return self.norm(tf.reduce_mean(pooled_output, axis=1))
+        return self.pool(pooled_output)
