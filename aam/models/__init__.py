96d60f9cfb41c102d5ed56d2390a91920a2187d1
Jonathan Kirkland <jokirkland@ucsd.edu>
2024-12-11 15:15:27 -0800

    added GOTUModel

--- aam/models/__init__.py
+++ aam/models/__init__.py
@@ -3,9 +3,11 @@ from __future__ import annotations
 from .base_sequence_encoder import BaseSequenceEncoder
 from .sequence_encoder import SequenceEncoder
 from .sequence_regressor import SequenceRegressor
+from .gotu_model import GOTUModel
 
 __all__ = [
     "BaseSequenceEncoder",
     "SequenceEncoder",
     "SequenceRegressor",
+    "GOTUModel",
 ]
