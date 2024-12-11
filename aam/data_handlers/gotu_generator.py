14ab7440947ca52bd6734a402b8b5e2dcf9b6135
Jonathan Kirkland <jokirkland@ucsd.edu>
2024-12-11 15:15:49 -0800

    commented out testing

--- aam/data_handlers/gotu_generator.py
+++ aam/data_handlers/gotu_generator.py
@@ -83,21 +83,21 @@ class GOTUGenerator(UniFracGenerator):
         return np.array([self.token_mapping[id] for id in obs_ids]).reshape((-1, 1))
 
 
-if __name__ == "__main__":
-    gotu_gen = GOTUGenerator(
-        table="/home/jokirkland/data/asv2gotu/rotation_results/tulsa1000/gotu_ordered_table_filtered.biom",
-        asv_table="/home/jokirkland/data/asv2gotu/rotation_results/tulsa1000/asv_ordered_table.biom",
-        tree_path="/home/jokirkland/data/trees/2022.10.phylogeny.asv.nwk",
-        metadata="/home/jokirkland/data/asv2gotu/rotation_results/tulsa1000/metag_metadata.tsv",
-        metadata_column="Age",
-        shift=0.0,
-        scale=100.0,
-        gen_new_tables=True,
-        is_16S=False,
-    )
-    print("Finished creating generator")
-    gotu_data = gotu_gen.get_data()
-    gotu_dataset = gotu_data["dataset"]
+# if __name__ == "__main__":
+#     gotu_gen = GOTUGenerator(
+#         table="/home/jokirkland/data/asv2gotu/rotation_results/tulsa1000/gotu_ordered_table_filtered.biom",
+#         asv_table="/home/jokirkland/data/asv2gotu/rotation_results/tulsa1000/asv_ordered_table.biom",
+#         tree_path="/home/jokirkland/data/trees/2022.10.phylogeny.asv.nwk",
+#         metadata="/home/jokirkland/data/asv2gotu/rotation_results/tulsa1000/metag_metadata.tsv",
+#         metadata_column="Age",
+#         shift=0.0,
+#         scale=100.0,
+#         gen_new_tables=True,
+#         is_16S=False,
+#     )
+#     print("Finished creating generator")
+#     gotu_data = gotu_gen.get_data()
+#     gotu_dataset = gotu_data["dataset"]
 
     # ug = UniFracGenerator(
     #     table="/home/jokirkland/data/asv2gotu/rotation_results/tulsa1000/asv_ordered_table.biom",
@@ -108,21 +108,21 @@ if __name__ == "__main__":
     #     scale=100.0,
     #     gen_new_tables=True,
     # )
-    # print("Finished creating 16S data")
     # asv_data = ug.get_data()
     # asv_dataset = asv_data["dataset"]
+    
     # for i, (x, y) in enumerate(asv_dataset):
     #     print("ASV_DATSET_VALUES")
     #     print(f"Printing X Data: {x}\nPrinting Y Data {y}")
     #     break
 
-    for i, (x, y) in enumerate(gotu_dataset):
-        print("GOTU_DATSET_VALUES")
-        print(f"Printing X Data: {x}\nPrinting Y Data {y}")
-        break
-    # full_dataset = tf.data.Dataset.zip((asv_dataset, gotu_dataset))
+#     for i, (x, y) in enumerate(gotu_dataset):
+#         print("GOTU_DATSET_VALUES")
+#         print(f"Printing X Data: {x}\nPrinting Y Data {y}")
+#         break
+#     # full_dataset = tf.data.Dataset.zip((asv_dataset, gotu_dataset))
 
-    # for i, (x, y) in enumerate(full_dataset):
-    #     print("FULL MERGED DATASET")
-    #     print(f"Printing X Data: {x}\nPrinting Y Data {y}")
-    #     break
+#     # for i, (x, y) in enumerate(full_dataset):
+#     #     print("FULL MERGED DATASET")
+#     #     print(f"Printing X Data: {x}\nPrinting Y Data {y}")
+#     #     break
