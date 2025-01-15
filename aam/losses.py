7bb294d3d8ee69e63f8ad045ecee7e8104f9f3db
kcantrel <kcantrel@ucsd.edu>
2025-01-15 10:54:01 -0800

    checkpoint

--- aam/losses.py
+++ aam/losses.py
@@ -42,6 +42,7 @@ def _pairwise_cosine_distance(embeddings):
     Args:
         embeddings (tf.Tensor): _description_
     """
+    embeddings = tf.linalg.l2_normalize(embeddings, axis=-1)
     distances = tf.matmul(embeddings, embeddings, transpose_b=True)
     return 1 - distances
 
@@ -99,4 +100,4 @@ def triplet_loss(embeddings, groups=2, margin=0.05):
     num_semi_hard_tuples = tf.reduce_sum(semi_hard_mask, axis=-1)
 
     semi_hard_loss = tf.math.divide_no_nan(tf.reduce_sum(triplet_loss * semi_hard_mask, axis=-1), num_semi_hard_tuples)
-    return tf.reduce_mean(semi_hard_loss) + global_embedding_l2_regulization(embeddings)
+    return tf.reduce_mean(semi_hard_loss)
