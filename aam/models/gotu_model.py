6347131fc87091d9d6b05c42d32477581873cf84
Jonathan Kirkland <jokirkland@ucsd.edu>
2025-02-07 16:39:59 -0800

    adding build method for inference and resuming model training

--- aam/models/gotu_model.py
+++ aam/models/gotu_model.py
@@ -61,6 +61,12 @@ class GOTUModel(tf.keras.Model):
         self._softmax = tf.keras.layers.Activation("softmax", dtype=tf.float32)
         self.gotu_output = tf.keras.layers.Dense(self.gotu_count)
 
+    def build(self, input_shape):
+        if self.built:
+            print("GOTU Model already built..")
+            return
+        super(GOTUModel, self).build(input_shape)
+
     def get_config(self):
         config = super(GOTUModel, self).get_config()
         config.update(
@@ -72,7 +78,8 @@ class GOTUModel(tf.keras.Model):
                 "intermediate_size": self.intermediate_size,
                 "intermediate_activation": self.intermediate_activation,
                 "gotu_count": self.gotu_count,
-                "base_model": self.base_model.get_config(),
+                "base_model": tf.keras.saving.serialize_keras_object(self.base_model),
+                "build_input_shape": self.get_build_config(),
             }
         )
         return config
@@ -80,35 +87,26 @@ class GOTUModel(tf.keras.Model):
     @classmethod
     def from_config(cls, config):
         asv_encoder_config = config.pop("base_model")
-        base_model = UnifracEncoder.from_config(asv_encoder_config)
+        base_model = tf.keras.saving.deserialize_keras_object(asv_encoder_config)
+        build_input_shape = config.pop("build_input_shape")
+        input_shape = build_input_shape["input_shape"]
         model = cls(base_model=base_model, **config)
-        token_shape = tf.TensorShape([None, 150])
-        batch_indices = tf.TensorShape([None, 2])
-        indices_shape = tf.TensorShape([None])
-        count_shape = tf.TensorShape([None, 1])
-        gotu_token_shape = tf.TensorShape([None])
-        gotu_batch_indices = tf.TensorShape([None, 2])
-        gotu_indices_shape = tf.TensorShape([None])
-        gotu_count_shape = tf.TensorShape([None, 1])
-
-        model.build(
-            (
-                [token_shape, batch_indices, indices_shape, count_shape],
-                [
-                    gotu_token_shape,
-                    gotu_batch_indices,
-                    gotu_indices_shape,
-                    gotu_count_shape,
-                ],
-            )
-        )
+        model.build(input_shape)
         return model
 
     def _compute_loss(self, data, outputs):
         inputs, targets = data
-        asv_inputs, gotu_inputs = inputs
+        (
+            asv_tokens,
+            asv_batch_indices,
+            asv_indicies,
+            asv_counts,
+            gotu_tokens,
+            gotu_batch_indices,
+            gotu_indicies,
+            gotu_counts,
+        ) = inputs
         asv_targets, gotu_targets = targets
-        (gotu_tokens, gotu_batch_indices, gotu_indicies, gotu_counts) = gotu_inputs
         gotu_tokens = tf.expand_dims(gotu_tokens, axis=-1)
         gotu_tokens, gotu_counts = self.base_model.batch_embeddings(
             gotu_tokens, gotu_batch_indices, gotu_counts, gotu_indicies
@@ -197,15 +195,55 @@ class GOTUModel(tf.keras.Model):
 
         return outputs[0]
 
+    def cast_inputs(self, inputs):
+        (
+            asv_tokens,
+            asv_batch_indices,
+            asv_indicies,
+            asv_counts,
+            gotu_tokens,
+            gotu_batch_indices,
+            gotu_indicies,
+            gotu_counts,
+        ) = inputs
+        asv_tokens = tf.cast(asv_tokens, dtype=tf.int32)
+        asv_batch_indices = tf.cast(asv_batch_indices, dtype=tf.int32)
+        asv_indicies = tf.cast(asv_indicies, dtype=tf.int32)
+        asv_counts = tf.cast(asv_counts, dtype=tf.int32)
+        gotu_tokens = tf.cast(gotu_tokens, dtype=tf.int32)
+        gotu_batch_indices = tf.cast(gotu_batch_indices, dtype=tf.int32)
+        gotu_indicies = tf.cast(gotu_indicies, dtype=tf.int32)
+        gotu_counts = tf.cast(gotu_counts, dtype=tf.int32)
+        return (
+            asv_tokens,
+            asv_batch_indices,
+            asv_indicies,
+            asv_counts,
+            gotu_tokens,
+            gotu_batch_indices,
+            gotu_indicies,
+            gotu_counts,
+        )
+
     def call(
         self,
         inputs,
         training: bool = False,
     ) -> tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
-        asv_inputs, gotu_inputs = inputs
-        (gotu_tokens, gotu_batch_indices, gotu_indicies, gotu_counts) = gotu_inputs
+        (
+            asv_tokens,
+            asv_batch_indices,
+            asv_indicies,
+            asv_counts,
+            gotu_tokens,
+            gotu_batch_indices,
+            gotu_indicies,
+            gotu_counts,
+        ) = self.cast_inputs(inputs)
         asv_embeddings, asv_counts = self.base_model.extract_asv_embeddings(
-            asv_inputs, batch_embeddings=True, sort_counts=True
+            (asv_tokens, asv_batch_indices, asv_indicies, asv_counts),
+            batch_embeddings=True,
+            sort_counts=True,
         )
         asv_mask = tf.cast(asv_counts > 0, dtype=self.compute_dtype)
 
