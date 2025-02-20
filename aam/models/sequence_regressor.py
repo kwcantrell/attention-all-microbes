44f0cd3678928f44665e5f32699e8ec4322cb059
Kalen Cantrell <cantrell.kalen@gmail.com>
2025-02-19 17:17:49 -0800

    checkpoint

--- aam/models/sequence_regressor.py
+++ aam/models/sequence_regressor.py
@@ -234,9 +234,10 @@ class SequenceRegressor(tf.keras.Model):
         # step 1: minimize mse
         mse_loss = tf.reduce_mean(tf.square(y_true - y_pred))
 
-        # step 2: pairwise distance of sample_embeddings should match pairwise distance of target
-        y_true_dist = _pairwise_distances(y_true, squared=False)
-        embedding_loss = tf.reduce_mean(self.embedding_loss(y_true_dist, sample_embeddings))
+        # # step 2: pairwise distance of sample_embeddings should match pairwise distance of target
+        # y_true_dist = _pairwise_distances(y_true, squared=False)
+        # embedding_loss = tf.reduce_mean(self.embedding_loss(y_true_dist, sample_embeddings))
+        embedding_loss = 0.0
         return mse_loss + embedding_loss, mse_loss, embedding_loss
 
     def _compute_metric(
