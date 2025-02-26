9333b455925f46b3f93016fc72a01f9225c3bacc
Kalen Cantrell <cantrell.kalen@gmail.com>
2025-02-26 14:41:24 -0800

    checkpoint

--- aam/triplet_pcoa.py
+++ aam/triplet_pcoa.py
@@ -25,12 +25,12 @@ taxonomy = pd.read_csv("agp-unique-samples-taxonomy.tsv", sep="\t", index_col=0)
 
 metadata = pd.read_csv(metadata_file, sep="\t", index_col=0, dtype={"#SampleID": str})
 
-# groups = []
-# for group, df in metadata.groupby(metadata_col):
-#     if df.shape[0] >= 10:
-#         groups.append(df)
-# metadata = pd.concat(groups)
-# metadata = metadata.groupby(metadata_col).sample(10, random_state=42, replace=False)
+groups = []
+for group, df in metadata.groupby(metadata_col):
+    if df.shape[0] >= 10:
+        groups.append(df)
+metadata = pd.concat(groups)
+metadata = metadata.groupby(metadata_col).sample(10, random_state=42, replace=False)
 print(metadata.shape, metadata[metadata_col].value_counts())
 model = tf.keras.models.load_model(model_path, compile=False)
 print("Model loaded!!")
@@ -50,10 +50,10 @@ ug = GeneratorDatasetV2(
 dataset = get_dataset(ug)
 
 
-def compute_sample_distances(sample_encodings):
-    sample_distances = np.square(
-        sample_encodings[np.newaxis, :, :] - sample_encodings[:, np.newaxis, :]
-    )
+def compute_sample_distances(x, y=None):
+    if y is None:
+        y = x
+    sample_distances = np.square(x[np.newaxis, :, :] - y[:, np.newaxis, :])
     sample_distances = np.sum(sample_distances, axis=-1)
     sample_distances = np.sqrt(sample_distances)
     return sample_distances
@@ -91,23 +91,38 @@ def _dot_prod(x, y=None):
     return np.sum(x * y, axis=-1)
 
 
+def _proj(u, v):
+    u_norms = np.sqrt(np.sum(u * u, axis=-1))
+    u /= u_norms
+    return v - _dot_prod(v, u) * u
+
+
 group_centers
 groups = list(group_centers.keys())
-anchor_group_center = group_centers[groups[0]]["center"]
+anchor_group = groups[0]
+anchor_group_center = group_centers[anchor_group]["center"]
 anchor_group_unit_center = anchor_group_center
-anchor_group_indices = group_centers[groups[0]]["indices"]
+anchor_group_indices = group_centers[anchor_group]["indices"]
 anchor_group_unit_center = anchor_group_unit_center / np.sqrt(
     np.sum(_dot_prod(anchor_group_unit_center))
 )
 for group in groups[1:]:
-    group_center = group_centers[group]["center"]
-    group_indices = group_centers[group]["indices"]
-    orthogal_direction = (
-        _dot_prod(group_center, anchor_group_unit_center) * anchor_group_unit_center
-    )
-    group_shift = group_center - orthogal_direction
-    sample_encodings[group_indices] -= group_shift
-sample_encodings -= anchor_group_center
+    anchor_points = sample_encodings[group_centers[anchor_group]["indices"]]
+    group_points = sample_encodings[group_centers[group]["indices"]]
+    group_distances = compute_sample_distances(group_points, anchor_points)
+    closest_anchors = np.argmin(group_distances, axis=-1)
+    group_projections = _proj(anchor_points[closest_anchors], group_points)
+    sample_encodings[group_centers[group]["indices"]] = group_projections
+    group_centers[anchor_group]["indices"] += group_centers[group]["indices"]
+
+    # group_center = group_centers[group]["center"]
+    # group_indices = group_centers[group]["indices"]
+    # orthogal_direction = (
+    #     _dot_prod(group_center, anchor_group_unit_center) * anchor_group_unit_center
+    # )
+    # group_shift = group_center - orthogal_direction
+    # sample_encodings[group_indices] -= group_shift
+# sample_encodings -= anchor_group_center
 
 sample_distances = compute_sample_distances(sample_encodings)
 num_samples = sample_distances.shape[0]
