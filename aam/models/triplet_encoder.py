51266e3943f7e88ff340c3ab64961b0f9d1a8348
kcantrel <kcantrel@ucsd.edu>
2025-02-26 04:20:47 -0800

    checkpoint

--- aam/models/triplet_encoder.py
+++ aam/models/triplet_encoder.py
@@ -79,8 +79,8 @@ class TripletEncoder(tf.keras.Model):
             ]
             return block
 
-        self.sample_embedding_ff = tf.keras.Sequential(_ff_block(32, dropout_rate=0.5))
-        self.tax_count_ff = tf.keras.Sequential(_ff_block(32, dropout_rate=0.5))
+        self.sample_embedding_ff = tf.keras.Sequential(_ff_block(128, dropout_rate=0.5))
+        self.tax_count_ff = tf.keras.Sequential(_ff_block(128, dropout_rate=0.5))
         self.out_ff = tf.keras.layers.Dense(32, use_bias=True)
         self.output_activation = tf.keras.layers.Activation("linear", dtype=tf.float32)
 
@@ -254,7 +254,7 @@ class TripletEncoder(tf.keras.Model):
         output_embedding = sample_embeddings + taxonomy_embeddings
         # output_embedding = self.out_ff(output_embedding)
         print("Triplet encoder exit...")
-        return self.output_activation(output_embedding)
+        return self.output_activation(self.out_ff(output_embedding))
 
     def get_config(self):
         config = super(TripletEncoder, self).get_config()
