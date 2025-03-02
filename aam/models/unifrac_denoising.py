b59be6847f3368908c53b8b1349ccd9ba3c5121c
Kalen Cantrell <cantrell.kalen@gmail.com>
2025-03-01 17:09:23 -0800

    checkpoint

--- aam/models/unifrac_denoising.py
+++ aam/models/unifrac_denoising.py
@@ -183,7 +183,7 @@ class UnifracDenoiser(tf.keras.Model):
         ],
     ):
         tokens = data
-        embeddings = self.asv_encoder.asv_embeddings(tokens)
+        embeddings = self.extract_asv_embeddings(tokens)
         return embeddings
 
     def train_step(
