93c31171ecbebc8a3e2a2163f458f5134b780c7a
kcantrel <kcantrel@ucsd.edu>
2025-03-23 00:21:30 -0700

    sequence embedding

---
+++ aam/data_handlers/sequence_embeddings.py
@@ -0,0 +1,45 @@
+import numpy as np
+from biom import Table
+
+
+class SequenceEmbeddings:
+    def __init__(self, embeddings_fp, labels_fp):
+        embeddings = np.load(embeddings_fp)
+        emb_mean = np.mean(embeddings, axis=0)
+        self.embeddings = embeddings - emb_mean
+        self.labels = labels_fp
+
+    def iget(self, indices):
+        return self.embeddings[indices]
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @labels.setter
+    def labels(self, fp):
+        labels = np.load(fp, allow_pickle=True)
+        self._labels = labels.astype(np.str_)
+
+    def __getitem__(self, indices):
+        return self.iget(indices)
+
+    def filter_table(self, table: Table):
+        table_asvs = table.ids(axis="observation")
+        keep = set(table_asvs).intersection(set(self.labels))
+        return table.filter(keep, axis="observation", inplace=False)
+
+    def align_table(self, table: Table):
+        """adds missing sequences and reorders table so that sequences are
+        in same order as self.labels"""
+
+        # add asvs that were dropped from the table during rarefaction
+        table_asvs = set(table.ids(axis="observation"))
+        missing_asv = set(self.labels).difference(table_asvs)
+        dropped_table = Table(
+            np.zeros((len(missing_asv), table.shape[1])),
+            list(missing_asv),
+            table.ids(),
+        )
+        table = table.concat(dropped_table, axis="observation")
+        return table.sort_order(self.labels, axis="observation")
