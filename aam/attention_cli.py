edc4c94c61864d01774ba5adce7da0c3b386e73f
Kalen Cantrell <cantrell.kalen@gmail.com>
2025-03-23 10:04:54 -0700

    checkpoint

--- aam/attention_cli.py
+++ aam/attention_cli.py
@@ -415,7 +415,7 @@ def fit_new_regressor(
 
     from aam.callbacks import LAMBLRScheduler, MeanAbsoluteError
     from aam.data_handlers.regressor_generator import RegressorGenerator
-    from aam.models.regressor_v2 import RegressorV2
+    from aam.models.regressor_v3 import RegressorV3
     from aam.models.utils import cos_decay_with_warmup
 
     # start pre processing dataset
@@ -473,8 +473,8 @@ def fit_new_regressor(
     if i_model is not None:
         model = tf.keras.models.load_model(i_model, compile=False)
     else:
-        # base_model = tf.keras.models.load_model(i_base_model, compile=False)
-        model = RegressorV2(train_gen.shift, train_gen.scale)
+        base_model = tf.keras.models.load_model(i_base_model, compile=False)
+        model = RegressorV3(base_model, train_gen.shift, train_gen.scale)
 
     token_shape = tf.TensorShape([None, 512])
     batch_indicies = tf.TensorShape([None, 2])
---
+++ aam/models/regressor_v3.py
@@ -0,0 +1,132 @@
+from __future__ import annotations
+
+from typing import Union
+
+import tensorflow as tf
+
+from aam.models.regressor_v2 import DenseBlock
+
+
+@tf.keras.saving.register_keras_serializable(package="RegressorV3")
+class RegressorV3(tf.keras.Model):
+    def __init__(self, base_model, shift, scale, num_encoder_layers=6, **kwargs):
+        super(RegressorV3, self).__init__(**kwargs)
+        self.loss_tracker = tf.keras.metrics.Mean(name="loss")
+        self.mae_tracker = tf.keras.metrics.Mean(name="mae")
+
+        self.num_encoder_layers = num_encoder_layers
+        self.shift = shift
+        self.scale = scale
+        self.base_model = base_model
+        self.base_model.trainable = False
+
+    def build(self, input_shape):
+        if self.built:
+            print("RegressorV3 is already built")
+            return
+
+        layers = []
+        for _ in range(self.num_encoder_layers):
+            layers.append(DenseBlock(512))
+        self.regressor = tf.keras.Sequential(
+            layers + [tf.keras.layers.Dense(1)], name="regressor"
+        )
+        super(RegressorV3, self).build(input_shape)
+
+    def predict_step(
+        self,
+        data: Union[
+            tuple[tuple[tf.Tensor, tf.Tensor], tf.Tensor],
+            tuple[tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor]],
+        ],
+    ):
+        inputs, y = data
+        output = self(inputs, training=False)
+
+        y = y * self.scale + self.shift
+        output = output * self.scale + self.shift
+        return output, y
+
+    def _compute_loss(self, y, output):
+        loss = tf.reduce_mean(tf.square(y - output))
+        return loss
+
+    def _compute_metric(self, y, output):
+        y = y * self.scale + self.shift
+        output = output * self.scale + self.shift
+        mae = tf.reduce_mean(tf.abs(y - output))
+        return mae
+
+    def train_step(
+        self,
+        data: Union[
+            tuple[tuple[tf.Tensor, tf.Tensor], tf.Tensor],
+            tuple[tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor]],
+        ],
+    ):
+        inputs, y = data
+
+        with tf.GradientTape() as tape:
+            output = self(inputs, training=True)
+            loss = self._compute_loss(y, output)
+        gradients = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+
+        mae = self._compute_metric(y, output)
+        self.loss_tracker.update_state(loss)
+        self.mae_tracker.update_state(mae)
+        return {
+            "loss": self.loss_tracker.result(),
+            "mae": self.mae_tracker.result(),
+            "learning_rate": self.optimizer.learning_rate,
+        }
+
+    def test_step(
+        self,
+        data: Union[
+            tuple[tuple[tf.Tensor, tf.Tensor], tf.Tensor],
+            tuple[tuple[tf.Tensor, tf.Tensor], tuple[tf.Tensor, tf.Tensor]],
+        ],
+    ):
+        inputs, y = data
+        output = self(inputs, training=False)
+        loss = self._compute_loss(y, output)
+        mae = self._compute_metric(y, output)
+        self.loss_tracker.update_state(loss)
+        self.mae_tracker.update_state(mae)
+        return {
+            "loss": self.loss_tracker.result(),
+            "mae": self.mae_tracker.result(),
+            "learning_rate": self.optimizer.learning_rate,
+        }
+
+    def call(self, inputs, training=False):
+        base_embeddings = self.base_model(inputs, training=training)
+
+        output = self.regressor(base_embeddings)
+        print("RegressorV3 exit...")
+        return output
+
+    def get_config(self):
+        config = super(RegressorV3, self).get_config()
+        config.update(
+            {
+                "shift": self.shift,
+                "scale": self.scale,
+                "num_encoder_layers": self.num_encoder_layers,
+                "build_input_shape": self.get_build_config(),
+            }
+        )
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        print("Constructing RegressorV3 from config")
+        input_shape = None
+        if "build_input_shape" in config:
+            build_input_shape = config.pop("build_input_shape")
+            input_shape = build_input_shape["input_shape"]
+
+        model = cls(**config)
+        model.build(input_shape)
+        return model
