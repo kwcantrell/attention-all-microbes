26f189161d8fade79e48c7af2b7edb0450dbe77e
kcantrel <kcantrel@ucsd.edu>
2025-03-23 19:44:25 -0700

    checkpoint

--- aam/attention_cli.py
+++ aam/attention_cli.py
@@ -440,7 +440,7 @@ def fit_new_regressor(
         "sequence_embeddings": i_sequence_embeddings,
         "sequence_labels": i_sequence_labels,
         "drop_remainder": False,
-        "normalize_embeddings": True,
+        "normalize_embeddings": False,
     }
 
     train_gen = RegressorGenerator(
