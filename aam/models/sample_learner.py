9c2d09fd701bede6df30e01b71550e35520df4de
kcantrel <kcantrel@ucsd.edu>
2025-05-08 04:05:31 -0700

    checkpoint

--- aam/models/sample_learner.py
+++ aam/models/sample_learner.py
@@ -294,13 +294,13 @@ class SampleLearner(tf.keras.Model):
 
         tie_ranks = rank_labels * tie_mask
         # tie_ranks = tf.where(tie_mask > 0, tie_ranks, self.rank_dim)
-        rank_labels = tf.math.reduce_max(
-            tie_ranks, axis=1
-        )  # - tf.math.floordiv(tf.reduce_sum(tie_mask), 2)
+        # rank_labels = tf.math.reduce_max(
+        #     tie_ranks, axis=1
+        # )  # - tf.math.floordiv(tf.reduce_sum(tie_mask), 2)
         # # # tf.print(rank_labels, counts)
-        # rank_labels = tf.math.ceil(
-        #     tf.reduce_sum(tie_ranks, axis=1) / tf.reduce_sum(tie_mask, axis=1)
-        # )
+        rank_labels = tf.math.ceil(
+            tf.reduce_sum(tie_ranks, axis=1) / tf.reduce_sum(tie_mask, axis=1)
+        )
         # rank_labels = tf.where(counts > 0, rank_labels, self.rank_dim - 1)
         rank_labels = tf.cast(rank_labels, dtype=tf.int32)
         return rank_labels
